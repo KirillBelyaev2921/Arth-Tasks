1110c7923d7e44a240430e9dcc852246e2941257
KirillBelyaev2921 <kirillbelyaev2921@gmail.com>
2024-10-07 19:03:31 +0300

    chore: init java spring project

---
+++ arth-tasks-manager/src/main/java/ua/arthead/artask/ArthTasksManagerApplication.java
@@ -0,0 +1,13 @@
+package ua.arthead.artask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArthTasksManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ArthTasksManagerApplication.class, args);
+  }
+
+}
---
+++ arth-tasks-manager/src/test/java/ua/arthead/artask/ArthTasksManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.arthead.artask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthTasksManagerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
